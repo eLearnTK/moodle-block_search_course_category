fcbeda99970463c2f4559f9039aeaf06b8b9badb
eLearnTK <Tobias.Kutzner@b-tu.de>
2017-01-05 17:32:36 +0100

    Add files via upload

---
+++ version.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/* Course Category Finder Block
+ * Course Category Finder is an extended version of the Course Finder Block. An Option is added to 
+ * enable the user to search for categories as well as the courses. 
+ * @package blocks
+ * @author: Tobias Kutzner, Igor Nesterow
+ */
+ 
+$plugin->version   = 2016120800;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2010112400;        // Requires this Moodle version
+$plugin->release = '3.0';
+$plugin->component = 'block_search_course_category';
+$plugin->maturity = MATURITY_ALPHA;
