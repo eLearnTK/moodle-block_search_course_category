4fb335d36992ee6854974347fb834d65ff8890e0
Tobias Kutzner <Tobias.Kutzner@b-tu.de>
2019-06-26 16:08:09 +0200

    Add files via upload

--- lang/en/block_search_course_category.php
+++ lang/en/block_search_course_category.php
@@ -24,8 +24,46 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+// String: "Common" .
 $string['pluginname'] = 'Course Category Finder';
 $string['search_course_category'] = 'Course Category Finder';
+$string['search'] = 'Search';
+
+// Strings: "block_search_course_category.php" .
 $string['course'] = "Course";
 $string['category'] = 'Category';
-$string['found'] = 'Categories found: ';
+$string['summary'] = 'Summary';
+
+// Strings: "course_form.php" .
+$string['searchcourses']            = 'Search term';
+
+$string['search_context']           = 'Search context';
+$string['combo_option_course']      = 'Search in courses';
+$string['combo_option_category']    = 'Search in categories';
+$string['combo_option_summary']     = 'Search in summary';
+
+$string['filtercheckbox']           = 'Disable all filters';
+$string['filterresults']            = 'Search Filter';
+$string['searchfromtime']           = 'Course start date';
+$string['searchtilltime']           = 'Course end date';
+
+$string['courseformat']             = 'Course format';
+$string['courseformat_none']        = 'Not set';
+$string['courseformat_topics']      = 'Topics';
+$string['courseformat_weeks']       = 'Weeks';
+$string['courseformat_activity']    = 'Single activity';
+
+$string['completioncriteria']           = 'Completion criteria';
+$string['completioncriteria_none']      = 'Not set';
+$string['completioncriteria_enable']    = 'Enable';
+$string['completioncriteria_disable']   = 'Disable';
+
+$string['sortheading']          = 'Sort Filter';
+$string['sortby']               = 'Sort by';
+$string['sortby_none']          = 'Not set';
+$string['sortby_fullname']      = 'Title';
+$string['sortby_shortname']     = 'Shortname';
+$string['sortby_startdate']     = 'Newest';
+
+// Strings: "view.php" .
+$string['no_results']     = 'No search results';
